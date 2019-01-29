08fa05c3c63b8bde1abede93b7e98851bcc35e5a
Nick Robin <nick.robin@braze.com>
2019-01-29 09:22:33 -0500

    keydown

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@
    //extra credit for keydown
    $(document).keydown((e) => {
      //check that the keypress is a letter so it doesn't reduce hearts when I press number
-     const alpha = new RegExp("[a-z]");
-     if (alpha.test(e.key)) {
+     //References the key values from here https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+     if (e.which >= 65 && e.which <= 90) {
       game.handleInteraction(e.key);
       }
    });
